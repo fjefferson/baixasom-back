3a912f9cf86fd32e24dd3557fca786d22d86b43c
Jefferson Oliveira <oliveirjefferson@gmail.com>
2026-01-28 23:48:36 -0300

    Remover arquivos de cloud deployment desnecessários

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Install system dependencies
-apt-get update
-apt-get install -y python3 python3-pip ffmpeg
-pip3 install -U yt-dlp
-
-# Install Node dependencies
-yarn install
